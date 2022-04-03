c845919f04d494f31f555273c01f53f015f48907
Dennis Prudlo <mail@dennisprudlo.com>
2022-04-03 19:08:07 +0200

    Add dkb bank statement converter

--- src/converters/bank-statement/pdf/DKBConverter.ts
+++ src/converters/bank-statement/pdf/DKBConverter.ts
@@ -37,7 +37,89 @@ export default class DKBConverter extends BankStatementConverter<PdfParsableFile
      * @returns The prepared context
      */
     public prepareToConvert () : Array<Array<TextItem>> {
-        return [];
+        let transactions: Array<Array<TextItem>> = [];
+
+        this.parsable.data().forEach(page => {
+            let startIndex = -1;
+            let endIndex = -1;
+
+            //
+            // Determine the indicies of the headers
+            for (let itemIndex = 0; itemIndex < page.contents.length; itemIndex++) {
+                const item = page.contents[itemIndex];
+
+                //
+                // The transactions have the text 'Wir haben für Sie gebucht', then an empty line, and then the transaction
+                // therefore the start index is 2 items after the text 'Wir haben für Sie gebucht'
+                if (item.str === 'Wir haben für Sie gebucht') {
+                    startIndex = itemIndex + 2;
+                    continue;
+                }
+
+                //
+                // The transaction listing on a page ends with the following items:
+                // ""
+                // "ALTER KONTOSTAND"
+                // "NEUER KONTOSTAND"
+                if (itemIndex > startIndex && item.str === 'ALTER KONTOSTAND') {
+                    const previousItem = page.contents[itemIndex - 1];
+                    const nextItem = page.contents[itemIndex + 1];
+                    if (previousItem.str.length === 0 && nextItem.str === 'NEUER KONTOSTAND') {
+                        endIndex = itemIndex - 1;
+                        break;
+                    }
+                }
+            }
+
+            //
+            // - If no start index was found the page does not have any transactions
+            // - If an end index was found, but the start was determined after the end index the page has no conventional listing
+            if (startIndex === -1 || (endIndex !== -1 && startIndex > endIndex)) {
+                return;
+            }
+
+            //
+            // If the endIndex was not found, use "undefined" to take all items until the end of the list
+            const cutoff = page.contents.slice(startIndex, endIndex === -1 ? undefined : endIndex);
+
+            let transactionBeginIndex = 0;
+            for (let itemIndex = 0; itemIndex < cutoff.length; itemIndex++) {
+                const item = cutoff[itemIndex];
+
+                //
+                // If the item found is a dkb date, the it may be the start of a new transaction.
+                // Transactions begin with the following item sequence: dkb-date, empty, dkb-date, empty
+                if (this.isFormattedDkbDate(item.str) && cutoff.length > (itemIndex + 3)) {
+                    const nextItem = cutoff[itemIndex + 1];
+                    const secondNextItem = cutoff[itemIndex + 2];
+                    const thirdNextItem = cutoff[itemIndex + 3];
+
+                    if (nextItem.str.length === 0 && this.isFormattedDkbDate(secondNextItem.str) && thirdNextItem.str.length === 0) {
+
+                        if (itemIndex === 0) {
+                            continue;
+                        }
+
+                        //
+                        // We found the start of a new transaction so we need to cutoff from the last begin until the previous item
+                        const range = cutoff.slice(transactionBeginIndex, itemIndex);
+                        transactions.push(range);
+
+                        //
+                        // Set the new transaction begin index
+                        transactionBeginIndex = itemIndex;
+                        continue;
+                    }
+                }
+            }
+
+            //
+            // After iteration we need to add the last chunk of items (the last transaction of the list)
+            const range = cutoff.slice(transactionBeginIndex);
+            transactions.push(range);
+        });
+
+        return transactions;
     }
 
     /**
@@ -46,7 +128,7 @@ export default class DKBConverter extends BankStatementConverter<PdfParsableFile
      * @returns The date of the transaction
      */
     public getBookedAt (context: Array<TextItem>) : string|null {
-        return null;
+        return context[0].str;
     }
 
     /**
@@ -55,7 +137,9 @@ export default class DKBConverter extends BankStatementConverter<PdfParsableFile
      * @returns The amount of the transaction
      */
     public getAmount (context: Array<TextItem>) : string {
-        return '';
+        console.table(context);
+        const isIncome = context[4].str === 'Zahlungseingang';
+        return (isIncome ? '' : '-') + context[context.length -1 ].str;
     }
 
     /**
@@ -64,7 +148,7 @@ export default class DKBConverter extends BankStatementConverter<PdfParsableFile
      * @returns The name of the transaction
      */
     public getName (context: Array<TextItem>) : string {
-        return '';
+        return context[5].str;
     }
 
     /**
@@ -73,6 +157,17 @@ export default class DKBConverter extends BankStatementConverter<PdfParsableFile
      * @returns The description of the transaction
      */
     public getDescription (context: Array<TextItem>) : string|null {
-        return null;
+        return context.filter((item, index) => index === 4 || (index > 5 && index < context.length - 1))
+            .map(item => item.str)
+            .join('\n') + '\nWertstellung: ' + context[2].str;
+    }
+
+    /**
+     * Checks if the string is a DKB bank statement formatted date (DD.MM.)
+     * @param string The string to check
+     * @returns True if the string is a DKB bank statement formatted date
+     */
+    private isFormattedDkbDate (string: string) : boolean {
+        return /^\d{2}\.\d{2}\.$/.test(string);
     }
 }
