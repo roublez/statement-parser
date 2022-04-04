08db95af617e01d59025aa2fc026b9c44de52cfe
Dennis Prudlo <mail@dennisprudlo.com>
2022-04-04 20:13:22 +0200

    Export errors

--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,20 @@
 import EntityType from "./enums/EntityType";
 import StatementParser from "./lib/StatementParser";
+import InvalidConverterMatchingError from "./errors/InvalidConverterMatchingError";
+import InvalidDataTransferError from "./errors/InvalidDataTransferError";
+import UnsupportedFileTypeError from "./errors/UnsupportedFileTypeError";
+
 import { GlobalWorkerOptions } from "pdfjs-dist";
 
 GlobalWorkerOptions.workerSrc = require('pdfjs-dist/build/pdf.worker.entry');
 
-export { StatementParser, EntityType };
+export {
+    StatementParser,
+    EntityType,
+
+    //
+    // Errors
+    InvalidConverterMatchingError,
+    InvalidDataTransferError,
+    UnsupportedFileTypeError
+};
